2c619659265b80f15198cb42612a477b6be6630e
Angie_Mejia <mejiagonzaleza881@gmail.com>
2022-09-15 20:09:22 -0500

    Primer commit :v

---
+++ src/Hola.java
@@ -0,0 +1,11 @@
+/**
+  * La clase hola construye un programa que
+ * muestra un mensaje en pantalla 
+ */
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola, ");
+        System.out.println("me llamo Jean");
+        System.out.println("Hasta luego");
+    }
+}
